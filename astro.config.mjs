8c24c7709b30f5ad1f882706e793180e133a88c9
Adan-Perez <itsadandev@gmail.com>
2025-09-03 22:14:32 +0100

    feat: update astro.config.mjs to include environment schema and configure node adapter Include prerenderer in index.astro for static page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, envField } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
+
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  env: {
+    schema: {
+      API_URL: envField.string({
+        context: 'server',
+        access: 'public',
+      }),
+    },
+  },
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 })
